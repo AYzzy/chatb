6b9672fbcddb4eed16f85296c144ff6a6126950a
AYzzy <ayomidedaniel798@gmail.com>
2025-03-12 12:17:23 +0100

    chat

--- src/main/java/com/chat/app/model/ChatMessage.java
+++ src/main/java/com/chat/app/model/ChatMessage.java
@@ -2,11 +2,41 @@ package com.chat.app.model;
 
 import lombok.*;
 
-@Data
-@NoArgsConstructor
+
 
 public class ChatMessage {
+    public ChatMessage(Long chatId, String sender, String message) {
+        this.chatId = chatId;
+        Sender = sender;
+        Message = message;
+    }
+
     private Long chatId;
+
+    public Long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(Long chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getSender() {
+        return Sender;
+    }
+
+    public void setSender(String sender) {
+        Sender = sender;
+    }
+
+    public String getMessage() {
+        return Message;
+    }
+
+    public void setMessage(String message) {
+        Message = message;
+    }
+
     private String Sender;
     private String Message;
 }
